2a0dec6e3500f0ab3e1ebdeaa6bccd1643eb6ee2
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-01 16:34:22 -0600

    Added test api with mock response for frontend dev team

---
+++ fake_thoughtful_events.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "faker"
+require 'pry'
+
+
+get '/events' do
+  genres = ["mexican","italian","sports","music","french"]
+  response = { genres: {}}
+
+  genres.each do |genre|
+    response[:genres][genre] = []
+    5.times do
+      response[:genres][genre] << {
+      name: Faker::Restaurant.name,
+      date:  Faker::Date.forward(days: rand(5..10)),
+      location: Faker::Address.street_address,
+      genre: genre
+      }
+    end
+  end
+  response.to_json
+end
