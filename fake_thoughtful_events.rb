eef262a746249da907b24eab28b28646be2136a4
Jesse Gietzen <gietzenjesse@gmail.com>
2020-06-02 10:01:08 -0600

    refactor hash instantiation with default value of empty array

--- fake_thoughtful_events.rb
+++ fake_thoughtful_events.rb
@@ -5,10 +5,9 @@ require 'pry'
 
 get '/events' do
   genres = ["mexican","italian","sports","music","french"]
-  response = { genres: {}}
+  response = { genres: Hash.new { |hash, key| hash[key] = [] } }
 
   genres.each do |genre|
-    response[:genres][genre] = []
     5.times do
       response[:genres][genre] << {
       name: Faker::Restaurant.name,
