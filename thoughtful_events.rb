9dfb3158ce844b030c4b63479ccaec71f92ce4ae
Jesse Gietzen <gietzenjesse@gmail.com>
2020-06-03 10:04:00 -0600

    add filler content

--- thoughtful_events.rb
+++ thoughtful_events.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'pry'
 
-  get '/' do
-    'Hello, World!'
-  end
+get '/events' do
+  content_type :json
+
+  {
+    music: [{name: 'Eagles',
+      location: '100 Circle Dr',
+      genre: 'music',
+      date: '10/10/99' }]
+  }.to_json
+end
+
+# url: https://app.ticketmaster.com/discovery/v2/
+# events.json?apikey={apikey}
