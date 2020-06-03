6cb40f92c317c1b08d047fe80358bb3647ba3347
Jesse Gietzen <gietzenjesse@gmail.com>
2020-06-03 11:08:00 -0600

    begin build ticketmaster service

---
+++ services/ticketmaster_service.rb
@@ -0,0 +1,17 @@
+class TicketmasterService
+  def events_by_genre_and_location(params)
+
+    conn = Faraday.new(url: 'https://app.ticketmaster.com/discovery/v2') do |f|
+      f.params['apikey'] = ENV['TICKETMASTER_API']
+    end
+
+    response = conn.get('events.json') do |req|
+      req.params['classificationName'] = params[:genre]
+      req.params['city'] = params[:city]
+      req.params['stateCode'] = params[:state]
+    end
+
+    json = JSON.parse(response.body)
+    binding.pry
+  end
+end
