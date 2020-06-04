cbd36d88d0c45f5711fd121bccb225eb4bd8597e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-03 21:01:23 -0600

    Add stub and remove WIP comments

--- spec/ticketmaster_spec.rb
+++ spec/ticketmaster_spec.rb
@@ -9,18 +9,12 @@ describe 'ticketmaster api stuffs' do
     }
     musicstub = File.read('spec/fixtures/ticketmaster_music.json')
     stub_request(:get, 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=9VxGzaXcajmAGvWV0Q3NYxP9sgNVXQct&classificationName=music&city=Denver&stateCode=CO').to_return(body: musicstub)
-    # stub_request(:get, 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=9VxGzaXcajmAGvWV0Q3NYxP9sgNVXQct&classificationName=sports&city=Denver&stateCode=CO').to_return(status: 405)
+    stub_request(:get, 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=9VxGzaXcajmAGvWV0Q3NYxP9sgNVXQct&classificationName=sports&city=Denver&stateCode=CO').to_return(body: musicstub)
     get '/events', query_params
 
     response_json = JSON.parse(last_response.body, symbolize_names: true)
-    first_music_event = response_json[:music].first
-    # expect(first_music_event).to eq(
-    #   {
-    #     name: 'Eagles',
-    #     location: '100 Circle Dr',
-    #     genre: 'music',
-    #     date: '10/10/99'
-    #   }
-    # )
+    expect(response_json[:music].count).to eq(20)
+    expect(response_json[:music][0][:name]).to eq("Juanes: Mas Futuro Que Pasado") 
+
   end
 end
