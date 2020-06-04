5754f781d3c66a5277a8a210091d30d3c3937025
Jesse Gietzen <gietzenjesse@gmail.com>
2020-06-04 13:36:12 -0600

    change double quotes to single quotes

--- serializers/ticketmaster_serializer.rb
+++ serializers/ticketmaster_serializer.rb
@@ -1,16 +1,17 @@
 class TicketmasterSerializer
   def ingest(response)
     events = []
-    response["_embedded"]["events"].each do |event|
+    response['_embedded']['events'].each do |event|
       location = {
-        address: event["_embedded"]["venues"][0]["city"]["name"] + ',' + event["_embedded"]["venues"][0]["state"]["stateCode"],
-        coordinates: event["_embedded"]["venues"][0]["location"]
+        address: event['_embedded']['venues'][0]['city']['name'] +
+        ',' + event['_embedded']['venues'][0]['state']['stateCode'],
+        coordinates: event['_embedded']['venues'][0]['location']
       }
       events << {
-        name: event["name"],
+        name: event['name'],
         location: location,
-        date: event["dates"]["start"]["localDate"],
-        genre: event["classifications"][0]["segment"]["name"]
+        date: event['dates']['start']['localDate'],
+        genre: event['classifications'][0]['segment']['name']
       }
     end
     events
