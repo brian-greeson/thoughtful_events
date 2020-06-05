a5d7097af0fe9c65b9b179486344a3c67c589c66
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-04 22:18:48 -0600

    Enable webmock for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,11 @@ require 'dotenv'
 Dotenv.load
 require 'rack/test'
 require 'rspec'
-# require 'webmock/rspec'
+require 'webmock/rspec'
 require './serializers/ticketmaster_serializer'
 require './services/ticketmaster_service'
 require './services/zomato_service'
-# WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: true)
 
 ENV['RACK_ENV'] = 'test'
 
